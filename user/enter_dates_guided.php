5356dfc3643c93149d4605487746760571b43ad8
benbarami <benbarami@gmail.com>
2019-02-22 14:17:23 +0200

    new changes

---
+++ user/enter_dates_guided.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+      <link rel="stylesheet" href="style/innerWindow.css" type="text/css">
+    <title></title>
+    <u>
+    <h2>Enter date range for existing tours</h2>
+  </u>
+  </head>
+  <body>
+  <br>
+  <form action="/action_page.php">
+  Starting Date: <input type="date" name="enter">
+  <br>
+  <br>
+  <br>
+  Finishing Date: <input type="date" name="finish">
+  <br>
+  <br>
+  <br>
+  Accessibility:
+  <input type="radio" name="radio" value="true"> Yes
+  <input type="radio" name="radio" value="false"> No
+  <br>
+  <br>
+  <br>
+  <input type="submit">
+</form>
+  </body>
+</html>

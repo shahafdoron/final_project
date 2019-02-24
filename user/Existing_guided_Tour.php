4da59ff9b8a1fc8eb14e8712f90efdd2ca294cda
benbarami <benbarami@gmail.com>
2019-02-24 19:14:05 +0200

    new screen

--- user/Existing_guided_Tour.php
+++ user/Existing_guided_Tour.php
@@ -22,7 +22,7 @@
       $enter_date=$_REQUEST['enter'];
       $finish_date=$_REQUEST['finish'];
       $is_access=$_REQUEST["access"];
-// להוסיף לשאילתה את היוזר ואת המדריך
+      // להוסיף לשאילתה את היוזר ואת המדריך
       $query= "select DATE_FORMAT(date(tour.planned_date_and_time_tour),'%d-%m-%Y') As tour_date,Time_Format(time(tour.planned_date_and_time_tour),
       '%k:%i') as Start_time ,tour.tour_duration,TIME_FORMAT(time(DATE_ADD(tour.planned_date_and_time_tour, INTERVAL tour.tour_duration MINUTE)),'%k:%i') as Finish_Time,
       tour.Description,(guided_tour.group_size-guided_tour.currently_participants) as remaining_tickets, guided_tour.registration_deadline,guided_tour.tour_cost
@@ -44,50 +44,28 @@
     ?>
     <div id=container class=container>
     </div>
+
     <script>
     var db= (<?php echo json_encode($dbdata); ?>);
     var counter=1;
     var txt="";
     for (i=0;i<db.length;i++){
-
+      txt+="<form method='POST' action='guided_info_book.php?tour="+JSON.stringify(db[i])+"'>";
       txt+="<div class='row' style='border-style: solid; border-width: 1px; padding: 1px 4px'>";
       txt+="<table style='width: 100%'>";
       txt+="<tr>";
-      txt+="<td style='text-align: center; text-decoration: underline;'>Tour number"+counter+"</td>";
-      txt+="<td style='text-align: center'>Date:"+db[i].tour_date+", Hours:"+db[i].Start_time+"-"+db[i].Finish_Time+"</td></tr>";
+      txt+="<td style='text-align: center; text-decoration: underline;'>Tour number "+counter+"</td>";
+      txt+="<td style='text-align: center'>Date : "+db[i].tour_date+", Hours: "+db[i].Start_time+"-"+db[i].Finish_Time+"</td></tr>";
       txt+="<tr><td>&nbsp;</td>";
-      txt+="<td style='text-align: center'>Cost:"+db[i].tour_cost+"₪<br><br>Remaining tickets:"+db[i].remaining_tickets+" Tickets</td></tr></table>";
+      txt+="<td style='text-align: center'>Cost: "+db[i].tour_cost+"₪ <br><br>Remaining tickets: "+db[i].remaining_tickets+" Tickets</td></tr></table>";
       txt+="<div class='btn' style='text-align: center'><input name='Submit1' type='submit' value='More information and booking' ></div>";
-      txt+="</div><div class='clear'></div>";
+      txt+="</div><div class='clear'></div></form>";
       counter+=counter;
 
     }
       document.getElementById("container").innerHTML=txt;
     </script>
 
-
-        <!--
-    // $hour="14:00";
-    //     $str.="<div class=\"container\">
-    // <div class=\"row\" style=\"border-style: solid; border-width: 1px; padding: 1px 4px\">
-    //
-    // 	<table style=\"width: 100%\">
-    // 	<tr>
-		// 		<td style=\"text-align: center\">Tour Number".$counter."</td>
-		// 		<td style=\"text-align: center\">Date: ".date( 'd/m/Y', $d ).", Hours:".$hour."</td>
-		// 	</tr>
-		// 	<tr>
-		// 		<td>&nbsp;</td>
-		// 		<td style=\"text-align: center\">cost:".$cost."<br><br>Remaining tickets:".$remaining." </td>
-		// 	</tr>
-		// </table>
-		//     <div class=\"btn\" style=\"text-align: center\"><input name=\"Submit1\" type=\"submit\" value=\"More information and booking\" ></div>
-    // </div>
-    // <div class='clear'></div>
-    // </div>";
-    // }
-    // $str.= "</div>";
-    // echo $str; -->
     <?php
     $result->free();
     $conn->close();
